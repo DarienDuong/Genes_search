ea37a784fee40c0a37b176caea27c20b2e720b97
Darien Duong <darienduong@gmail.com>
2018-04-11 19:18:34 -0700

    modified filter for gene-search

--- gene_app/views.py
+++ gene_app/views.py
@@ -3,18 +3,13 @@ from django.http import JsonResponse
 from gene_app.serializers import GeneSerializer, GeneSearchSerializer
 from django.db.models import Count
 
-from django.http import HttpResponse
-
-# Create your views here.
 def home(request):
-	# genes = Gene.objects.values('gene').annotate(number_genes=Count('gene'))
-	# print(genes)
 	query = Gene.objects.values('gene').annotate(number_genes=Count('gene')).order_by('gene')
 	genes = GeneSerializer(query, many = True).data
 	return JsonResponse({"genes": genes})
 
 def gene_search(request, name):
-	# consider using __iexact
-	search_query = Gene.objects.values('gene').filter(gene__icontains=name).order_by('gene')
+	# consider using __icontains
+	search_query = Gene.objects.values('gene').filter(gene__iexact=name).order_by('gene')
 	variants = GeneSearchSerializer(search_query, many=True).data
 	return JsonResponse({"variants": variants})
