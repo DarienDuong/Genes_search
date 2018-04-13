4624ee9668074ca80dc22f26977ffa0e179565b7
Darien Duong <darienduong@gmail.com>
2018-04-13 14:39:43 -0700

    fixed style for auto section

--- src/AutoGene.js
+++ src/AutoGene.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    border-style: solid;
+    border-width: 2px 2px 2px 2px;
+    margin-top: 0px;
+`
+
+const StyledP = styled.p`
+    text-align: center;
+`
 
 const AutoGene = ({ gene }) => {
     return (
-        
+        <StyledDiv>
+            <StyledP>{gene}</StyledP>
+        </StyledDiv>
     )
 }
 
